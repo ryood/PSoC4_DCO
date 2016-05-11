2d07f9123b93078d6db7e7fa467431c432600942
ryood <kawanabe@gmail.com>
2016-05-11 13:34:54 +0900

    Add: PSoC4_SPI_Slave_Test Project

--- PSoC/I2C_Slave_Test.cydsn/main.c
+++ PSoC/I2C_Slave_Test.cydsn/main.c
@@ -11,16 +11,148 @@
 */
 #include <project.h>
 
+
+/***************************************
+*         Function Prototypes
+****************************************/
+
+uint8 ExecuteCommand(uint32 cmd);
+
+
+/***************************************
+*            Constants
+****************************************/
+
+/* Buffer and packet size */
+#define BUFFER_SIZE      (6u)
+#define PACKET_SIZE      (BUFFER_SIZE / 2u)
+#define READ_ONLY_OFFSET (3u)
+
+/* Packet positions */
+#define PACKET_SOP_POS  (0u)
+#define PACKET_CMD_POS  (1u)
+#define PACKET_STS_POS  (4u)
+#define PACKET_EOP_POS  (2u)
+
+/* Start and end of packet markers */
+#define PACKET_SOP      (0x01u)
+#define PACKET_EOP      (0x17u)
+
+/* Command valid status */
+#define STS_CMD_DONE    (0x00u)
+#define STS_CMD_FAIL    (0xFFu)
+
+/* Commands set */
+#define CMD_SET_OFF     (0u)
+#define CMD_SET_RED     (1u)
+#define CMD_SET_GREEN   (2u)
+#define CMD_SET_BLUE    (3u)
+
+
+/***************************************
+*               Macros
+****************************************/
+
+/* Set LED RED color */
+#define RGB_LED_ON_RED  \
+                do{     \
+                    LED_RED_Write  (0u); \
+                    LED_GREEN_Write(1u); \
+                    LED_BLUE_Write (1u); \
+                }while(0)
+
+/* Set LED GREEN color */
+#define RGB_LED_ON_GREEN \
+                do{      \
+                    LED_RED_Write  (1u); \
+                    LED_GREEN_Write(0u); \
+                    LED_BLUE_Write (1u); \
+                }while(0)
+
+/* Set LED BLUE color */
+#define RGB_LED_ON_BLUE \
+                do{     \
+                    LED_RED_Write  (1u); \
+                    LED_GREEN_Write(1u); \
+                    LED_BLUE_Write (0u); \
+                }while(0)
+
+/* Set LED TURN OFF */
+#define RGB_LED_OFF \
+                do{ \
+                    LED_RED_Write  (1u); \
+                    LED_GREEN_Write(1u); \
+                    LED_BLUE_Write (1u); \
+                }while(0)
+
+/* EZI2C slave buffer for write and read */
+uint8 ezI2cBuffer[BUFFER_SIZE] = {PACKET_SOP, STS_CMD_FAIL, PACKET_EOP, \
+                                  PACKET_SOP, STS_CMD_FAIL, PACKET_EOP};
+
 int main()
 {
-    CyGlobalIntEnable; /* Enable global interrupts. */
+    uint8 status = STS_CMD_FAIL;
 
-    /* Place your initialization/startup code here (e.g. MyInst_Start()) */
+    RGB_LED_OFF;
+    
+    /* Setup buffer and start EZI2C slave (SCB mode) */
+    EZI2C_EzI2CSetBuffer1(BUFFER_SIZE, READ_ONLY_OFFSET, ezI2cBuffer);
+    EZI2C_Start();
+    
+    CyGlobalIntEnable; /* Enable global interrupts. */
 
     for(;;)
     {
-        /* Place your application code here. */
+        /* Write complete: parse packet */
+        if (0u != (EZI2C_EzI2CGetActivity() & EZI2C_EZI2C_STATUS_WRITE1))
+        {
+            /* Check start and end of packet markers */
+            if ((ezI2cBuffer[PACKET_SOP_POS] == PACKET_SOP) &&
+                (ezI2cBuffer[PACKET_EOP_POS] == PACKET_EOP))
+            {
+                status = ExecuteCommand(ezI2cBuffer[PACKET_CMD_POS]);
+            }
+
+            /* Update buffer with status */
+            ezI2cBuffer[PACKET_STS_POS] = status;
+            status = STS_CMD_FAIL;
+        }
+
+        /* Buffer is always available to be read */
     }
+}
+
+uint8 ExecuteCommand(uint32 cmd)
+{
+    uint8 status;
+
+    status = STS_CMD_DONE;
+
+    /* Execute received command */
+    switch (cmd)
+    {
+        case CMD_SET_RED:
+            RGB_LED_ON_RED;
+            break;
+
+        case CMD_SET_GREEN:
+            RGB_LED_ON_GREEN;
+            break;
+
+        case CMD_SET_BLUE:
+            RGB_LED_ON_BLUE;
+            break;
+
+        case CMD_SET_OFF:
+            RGB_LED_OFF;
+            break;
+
+        default:
+            status = STS_CMD_FAIL;
+        break;
+    }
+
+    return(status);
 }
 
 /* [] END OF FILE */
---
+++ PSoC/PSoC4_SPI_Slave_Test.cydsn/main.c
@@ -0,0 +1,111 @@
+/* ========================================
+ *
+ * SPI Slaveの送受信テスト
+ * 
+ * 2016.05.09
+ *
+ * ========================================
+*/
+#include <project.h>
+#include <stdio.h>
+
+#define RX_PACKET_SIZE  3
+#define TX_PACKET_SIZE  3
+
+// Prototype
+int readRE();
+
+int main()
+{
+    int i;
+    int txData;
+    uint8 rxBuffer[RX_PACKET_SIZE];
+    uint8 txBuffer[TX_PACKET_SIZE];
+    
+    char strBuffer[80];
+    
+    CyGlobalIntEnable; /* Enable global interrupts. */
+
+    UART_Start();
+    UART_UartPutString("SPI Slave test.\r\n");
+    
+    LCD_Char_Start();
+    LCD_Char_PrintString("SPI Slave Test");
+
+    SPIS_Start();
+
+    txData = 0;
+    for(;;)
+    {
+        txData += readRE();
+        
+        /*
+        while (RX_PACKET_SIZE > SPIS_SpiUartGetRxBufferSize())
+        {
+            txData += readRE();
+        }
+        */
+        if (RX_PACKET_SIZE <= SPIS_SpiUartGetRxBufferSize()) {
+            // RX
+            for (i = 0; i < RX_PACKET_SIZE; i++) {
+                rxBuffer[i] = SPIS_SpiUartReadRxData();
+            }
+
+            // TX
+            txBuffer[0] = rxBuffer[0];
+            txBuffer[1] = txData >> 8;
+            txBuffer[2] = txData & 0xff;
+            SPIS_SpiUartPutArray(txBuffer, TX_PACKET_SIZE);
+            
+            //sprintf(strBuffer, "RX: %03u %03u %03u\r\n", rxBuffer[0], rxBuffer[1], rxBuffer[2]);
+            sprintf(strBuffer, "RX: %03u %4d   \r\n", rxBuffer[0], ((uint16)rxBuffer[1] << 8) | rxBuffer[2]);
+            UART_UartPutString(strBuffer);
+            
+            LCD_Char_ClearDisplay();
+            strBuffer[15] = 0;
+            LCD_Char_Position(0, 0);
+            LCD_Char_PrintString(strBuffer);
+            
+            sprintf(strBuffer, "TX: %03u %03u %03u\r\n", txBuffer[0], txBuffer[1], txBuffer[2]);
+            //sprintf(strBuffer, "txData:%d    \r\n", txData);
+            UART_UartPutString(strBuffer);
+
+            strBuffer[15] = 0;
+            LCD_Char_Position(1, 0);
+            LCD_Char_PrintString(strBuffer);
+        }
+        //CyDelay(1);
+    }
+}
+
+//-------------------------------------------------
+// ロータリーエンコーダの読み取り
+// return: ロータリーエンコーダーの回転方向
+//         0:変化なし 1:時計回り -1:反時計回り
+//
+int readRE()
+{
+    static uint8_t index;
+    uint8_t rd = 0;
+    int retval = 0;
+    
+    rd = Pin_RE_Read();
+    index = (index << 2) | rd;
+	index &= 0b1111;
+
+	switch (index) {
+	// 時計回り
+	case 0b0001:	// 00 -> 01
+	case 0b1110:	// 11 -> 10
+	    retval = 1;
+	    break;
+	// 反時計回り
+	case 0b0010:	// 00 -> 10
+	case 0b1101:	// 11 -> 01
+	    retval = -1;
+	    break;
+    }
+    return retval;
+}
+
+/* [] END OF FILE */
